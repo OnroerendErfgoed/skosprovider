63cdf8aac44d615a780c068635cc0b110a2c3964
Koen Van Daele <koen_van_daele@telenet.be>
2013-12-10 16:42:06 +0100

    Add some documentation to the uri module.

--- skosprovider/uri.py
+++ skosprovider/uri.py
@@ -29,6 +29,12 @@ class UriPatternGenerator(UriGenerator):
         self.pattern = pattern
 
     def generate(self, **kwargs):
+        '''
+        Generate a :term:`URI` based on parameters passed.
+
+        :param id: The id of the concept or collection.
+        :rtype: string
+        '''
         return self.pattern % kwargs['id']
 
 
@@ -48,6 +54,12 @@ class DefaultUrnGenerator(UriGenerator):
         self.vocabulary_id = vocabulary_id
 
     def generate(self, **kwargs):
+        '''
+        Generate a :term:`URI` based on parameters passed.
+
+        :param id: The id of the concept or collection.
+        :rtype: string
+        '''
         return (self.pattern % (self.vocabulary_id, kwargs['id'])).lower()
 
 
@@ -65,6 +77,14 @@ class TypedUrnGenerator(DefaultUrnGenerator):
         self.vocabulary_id = vocabulary_id
 
     def generate(self, **kwargs):
+        '''
+        Generate a :term:`URI` based on parameters passed.
+
+        :param id: The id of the concept or collection.
+        :param type: What we're generating a :term:`URI` for: `concept` 
+            or `collection`.
+        :rtype: string
+        '''
         if kwargs['type'] not in ['concept', 'collection']:
             raise ValueError('Type %s is invalid' % kwargs['type'])
         return (self.pattern % (self.vocabulary_id, kwargs['type'], kwargs['id'])).lower()
