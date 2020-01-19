0870960f6f3ab46b20d5aeb947c8fc6c5878e782
Koen Van Daele <koen_van_daele@telenet.be>
2020-01-19 20:45:14 +0100

    Drop support for Python 3.5 and add 3.8

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     test_suite='nose.collector'
 )
